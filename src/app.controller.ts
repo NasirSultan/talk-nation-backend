90bfa22a1198ebf208e6523df5ff4273e327eefc
NasirSultan <rainasirsultan123@gmail.com>
2025-09-13 11:06:12 +0500

    s

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api') // <-- Prefix all routes with /api
+@Controller() // <-- Prefix all routes with /api
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
